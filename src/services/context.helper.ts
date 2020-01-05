fdf0d41502072791ddda884f54876235b0839b6f
MrWhite <hugoblanc.blend@gmail.com>
2020-01-05 13:17:39 +0100

    feat: persitente contexte helper

---
+++ src/services/context.helper.ts
@@ -0,0 +1,49 @@
+import * as vscode from 'vscode';
+
+export class ContextHelper {
+
+    public static instance: ContextHelper;
+    public static MACHINE_KEY_ARRAY = 'MACHINE_KEY_ARRAY';
+
+    public static getInstance(context?: vscode.ExtensionContext) {
+        if (!this.instance && context) {
+            this.instance = new ContextHelper(context);
+        }
+        return this.instance;
+    }
+
+    private constructor(private context: vscode.ExtensionContext) {
+    }
+
+
+    public getObjectsFromArrayKey(arrayKey: string){
+        const array = this.getJson(arrayKey, '[]') as string[];
+        return array.map((key)=>this.getJson(key,undefined));        
+    }
+
+    public async push(arrayKey: string, newValue: string){
+        const array = this.getJson(arrayKey, '[]') as string[];
+        if(array.indexOf(newValue)===-1){
+            array.push(newValue);
+            await this.setJson(arrayKey, array);
+        }
+    }
+
+    public getJson(key: string, defaultValue?: string | any) {
+        return JSON.parse(this.get(key, defaultValue));
+    }
+
+    public setJson(key: string, object:any){
+        return this.set(key, JSON.stringify(object));
+    }
+
+    public get(key: string, defaultValue: string | any = '') {
+        return this.context.globalState.get(key, defaultValue);
+
+    }
+
+    public set(key: string, value: string) {
+        return this.context.globalState.update(key, value);
+    }
+
+}
